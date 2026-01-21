097fec1f58bc7e6858ae58e81d741b3e17a4a809
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2026-01-21 23:49:56 +0000

    Add checkoutAccountType.h

---
+++ checkingAccountType.h
@@ -0,0 +1,6 @@
+#ifndef CHECKING_ACCOUNT_TYPE_H
+#define CHECKING_ACCOUNT_TYPE_H
+class checkingAccountType {
+    checkingAccountType::checkingAccountType(std::string name, int accountNumber, int accountBalance);
+}
+#endif // CHECKING_ACCOUNT_TYPE_H  
