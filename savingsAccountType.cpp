817c6ce7adbedc48901335f6a6244c214e7dbc75
RyanKPosey <157773263+RyanKPosey@users.noreply.github.com>
2026-01-21 23:44:27 +0000

    Add savignsAccountType.h/.cpp

---
+++ savingsAccountType.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "savingsAccountType.h"
+
+savingsAccountType::savingsAccountType(std::string name, int accountNumber, int accountBalance) {
+    cout << "savingsAccountType constructor" << endl;
+}
---
+++ savingsAccountType.h
@@ -0,0 +1,8 @@
+#ifndef SAVINGS_ACCOUNT_TYPE_H
+#define SAVINGS_ACCOUNT_TYPE_H
+
+class savingsAccountType {
+    savingsAcoountType::savingsAccountType(std::string name, int accountNumber, int accountBalance);
+}
+
+#endif // SAVINGS_ACOCUNTS_TYPE_H
