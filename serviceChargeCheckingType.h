f3d3fd7946a67531a389c84d37b3bfcdb5b870ef
Chaoqi XIng <chaoqixing1002@gmail.com>
2026-01-22 15:23:03 -0800

    serviceChargeCheckingType.h

--- serviceChargeCheckingType.h
+++ serviceChargeCheckingType.h
@@ -1,1 +1,19 @@
+#ifndef SERVICE_CHARGE_CHECKING_TYPE_H
+#define SERVICE_CHARGE_CHECKING_TYPE_H
 
+#include "bankAccountType.h"
+#include <string>
+
+class serviceChargeCheckingType : public bankAccountType
+{
+public:
+    serviceChargeCheckingType(const std::string& name, int accountNumber, double balance);
+
+    void withdraw(double amount) override;
+    void print() const override;
+
+private:
+    double serviceFee_ = 5.00;
+};
+
+#endif
