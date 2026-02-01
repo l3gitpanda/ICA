e4acf045bb038fdd51d50b88cfc0a4163139cf16
l3gitpanda <callofdutyguy4@gmail.com>
2026-02-01 11:42:13 -0800

    Revert "Add in-memory account database and savings account interface"

--- savingsAccountType.cpp
+++ savingsAccountType.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
 #include "savingsAccountType.h"
 
-savingsAccountType::savingsAccountType(const std::string& name,
-                                       int accountNumber,
-                                       double accountBalance,
-                                       const std::string& userId)
-    : bankAccountType(name, accountNumber, accountBalance),
-      userId_(userId)
+savingsAccountType::savingsAccountType(const std::string& name, int accountNumber, double accountBalance)
+    : bankAccountType(name, accountNumber, accountBalance)
 {
-}
-
-std::string savingsAccountType::getUserId() const
-{
-    return userId_;
-}
-
-void savingsAccountType::print() const
-{
-    std::cout << "[savingsAccountType]\n";
-    bankAccountType::print();
-    std::cout << "User ID: " << userId_ << "\n";
+    std::cout << "savingsAccountType constructor" << std::endl;
 }
